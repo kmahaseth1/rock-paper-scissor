146872e440da8621ead4f653786de5d632d2f078
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-01-25 12:32:49 -0500

    Add a function to handle the player's move
    Add a function that will allow the player to chose their move and ensure that it is a valid move

--- script.js
+++ script.js
@@ -2,16 +2,43 @@
 
 function getComputerChoice () {
 
-    let raw_choice = Math.random()
-    let final_choice
+    let rawChoice = Math.random()
+    let finalChoice
 
-    if(raw_choice <= 0.33) {
-        final_choice = "Rock";
-    } else if (raw_choice <= 0.67) {
-        final_choice = "Paper"
+    if(rawChoice <= 0.33) {
+        finalChoice = "Rock"
+    } else if (rawChoice <= 0.67) {
+        finalChoice = "Paper"
     } else {
-        final_choice = "Scissors"
+        finalChoice = "Scissors"
     }
 
-    return final_choice
+    return finalChoice
+}
+
+// Function to obtain player's choice
+function getHumanChoice() {
+
+    let valid_responses = ["rock", "paper", "scissors"]
+    let response = prompt("What would you like to play?")
+
+    if(!valid_responses.includes(response.toLowerCase())) {
+        response = prompt(
+            "Invalid move.\n" +
+            "Please check your spelling and reenter your response."
+            )
+        getHumanChoice()
+    } else {
+        return response
+    }
+    
+}
+
+// Initialize variables to hold the scores
+let machineScore = 0;
+let playerScore = 0;
+
+// Function to play a round of the game
+function playRound(getComputerChoice, getHumanChoice) {
+
 }
